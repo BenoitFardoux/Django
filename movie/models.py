19fb71c1fe9f024aa2aa8f323584641b693dc8f5
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2024-05-15 11:33:52 +0200

    feat(search movie): update models

--- movie/models.py
+++ movie/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     release_date = models.DateField()
-    poster = models.ImageField()
+    poster = models.CharField(max_length=500)
     genre = models.CharField(max_length=500)
