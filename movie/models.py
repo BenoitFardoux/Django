78ee61d28fbb442530374170401de74288dc0225
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2024-05-15 09:41:22 +0200

    remove database from github

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    genre = models.ManyToManyField(Genre)
+
