bfe55c3fb3e4e41e19ba291a89f5fcaad4464c20
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2024-05-15 10:29:29 +0200

    add search view

--- movie/views.py
+++ movie/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
+from movie.models import Movie
+
 
 def index(request):
     template = loader.get_template("movie/index.html")
     context = {}
     return HttpResponse(template.render(context, request))
+
+
+def search_movie(request,movie_name):
+    status = 200
+    context = {}
+    if request.method != 'GET':
+        status = 405
+    else:
+        movies = Movie.objects.filter(title__icontains=movie_name)
+        context = {'movies':movies}
+    return render(request, 'movie/index.html', context, status=status)
