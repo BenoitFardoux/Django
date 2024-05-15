41f142e81f75801ba023bbe9f07023ad1782c817
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2024-05-15 11:26:11 +0200

    feat(search movie): add external API Call

--- movie/models.py
+++ movie/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 
 
-# Create your models here.
-class Genre(models.Model):
-    name = models.CharField(max_length=200)
-
-    @property
-    def get_all_movies(self):
-        return self.movie_set.all().values_list()
-
-
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     release_date = models.DateField()
-    genre = models.ManyToManyField(Genre)
+    poster = models.ImageField()
+    genre = models.CharField(max_length=500)
